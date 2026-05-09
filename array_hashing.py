43499487090f2bd6f9a00b2c81b8ebe905bf571c
femiayodeji <femidotayodeji@gmail.com>
2026-05-09 17:37:05 +0100

    solve two sum problem using hashmap

--- array_hashing.py
+++ array_hashing.py
@@ -22,3 +22,20 @@ class ProductArrayExceptSelfNoDivision:
             suffix *= nums[i]
 
         return products
+
+class TwoSum:
+    def solve(nums = [2, 7, 11, 15], target = 9):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+    def optimal(nums = [2, 7, 11, 15], target = 9):
+        seen = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in seen:
+                return [seen[diff], i]
+            seen[nums[i]] = i 
+        return []
