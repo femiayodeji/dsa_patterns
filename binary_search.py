e244fe3ac35d23bab660838039eb222db9be6d90
femiayodeji <femidotayodeji@gmail.com>
2026-05-12 21:05:25 +0100

    Solve search rotated sorted array using binary search

--- binary_search.py
+++ binary_search.py
@@ -120,3 +120,30 @@ class RotatedSortedArray:
                 
         return nums[left]
             
+class SearchRotatedSortedArray:
+    def brute(nums = [4, 5, 6, 7, 0, 1, 2],  target = 0):
+        for k,v in enumerate(nums):
+            if nums[k] == target:
+                return k
+        return -1
+
+    def optimal(nums = [4, 5, 6, 7, 0, 1, 2], target = 0):
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[right] > nums[mid]:
+                if target >= nums[mid] and target < nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            else:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        
+        return -1
