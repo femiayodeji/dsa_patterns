48358b97ec14a35841033d729c4ae4d6cd055d0a
femiayodeji <femidotayodeji@gmail.com>
2026-05-12 19:26:33 +0100

    Solve matrix search with binary search

--- binary_search.py
+++ binary_search.py
@@ -20,3 +20,48 @@ class BinarySearch:
                 right = mid - 1
             
         return -1
+
+class MatrixSearch:
+    def solve(matrix = [
+        [1,  3,  5,  7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 60]
+    ],
+    target = 3):
+        
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if matrix[row][col] == target:
+                    return True
+
+        return False
+
+    def optimal(matrix = [
+        [1,  3,  5,  7],
+        [10, 11, 16, 20],
+        [23, 30, 34, 60]
+    ],
+    target = 3):
+        import math
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        left = 0
+        right = rows * rows - 1
+        
+        while (left <= right):
+            mid = (left + right)//2
+            row = math.floor(mid/cols)
+            col = mid % cols
+            value = matrix[row][col]
+            
+            if value == target:
+                return True
+            elif target > value:
+                left = mid + 1
+            else:
+                right = mid - 1
+            
+        
+        return False
